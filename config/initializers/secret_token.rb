7591888eaf2ee9b90cba43a128391a4df27ab36d
Elomar Nascimento dos Santos <elomarns@gmail.com>
2012-06-25 15:34:25 -0300

    First commit! \o/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikilicious::Application.config.secret_token = 'e4b575e997df71fb7fbae5e6c1a6703755c1ff72f8e50c0445439a497018cb048117970d95a4efe6377af5a4b3f82363c714389b727bf67e3851620f4fae396d'
