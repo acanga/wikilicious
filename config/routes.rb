4aac9a0d3833f17d32c15fc23f130223188871b8
Elomar Nascimento dos Santos <elomarns@gmail.com>
2012-06-28 09:03:54 -0300

    More routes = more fun.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Wikilicious::Application.routes.draw do
   devise_for :users
+  post "/users/sign_up" => "devise/registrations#new"
+  post "/users/sign_in" => "devise/sessions#new"
 
-  # root :to => 'welcome#index'
+  root :to => "high_voltage/pages#show", :id => "index"
 end
