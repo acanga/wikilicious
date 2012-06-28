ab4b6f051057e205277fee26505f352965083f33
Elomar Nascimento dos Santos <elomarns@gmail.com>
2012-06-28 10:00:29 -0300

    Ops. Routes file went off the rails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Wikilicious::Application.routes.draw do
-  devise_for :users
-  post "/users/sign_up" => "devise/registrations#new"
-  post "/users/sign_in" => "devise/sessions#new"
+  devise_for :users do
+    post "/users/sign_up" => "devise/registrations#create", :as => :sign_up
+    post "/users/sign_in" => "devise/sessions#create", :as => :sign_in
+  end
 
   root :to => "high_voltage/pages#show", :id => "index"
 end
