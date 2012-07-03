a486432a5f5e430a5e6626257a6f7cf0edbbb0d8
Elomar Nascimento dos Santos <elomarns@gmail.com>
2012-07-03 09:17:35 -0300

    Forgot your password?

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Wikilicious::Application.routes.draw do
   devise_for :users do
     post "/users/sign_up" => "devise/registrations#create", :as => :sign_up
     post "/users/sign_in" => "devise/sessions#create", :as => :sign_in
+    post "/users/password/new" => "devise/passwords#create", :as => :new_password
+    put "/users/password/edit" => "devise/passwords#update", :as => :edit_password
   end
 
   root :to => "high_voltage/pages#show", :id => "index"
